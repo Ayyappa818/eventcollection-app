d9304fa89ce31a7b5785529c4de6bdd89f62236e
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-12-05 12:44:17 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+export default App;
