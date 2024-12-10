c785c2fb0906bbaefdc075b768b5965e440ba31a
Ayyappa818 <ayyappa666swamy@gmail.com>
2024-12-10 20:49:25 +0530

    changes

--- src/features/Manager/Movies/SideMoviebar.jsx
+++ src/features/Manager/Movies/SideMoviebar.jsx
@@ -5,7 +5,7 @@ function SideMoviebar() {
     <div class='w-100 p-2 m-2'>
         <div><h3>Filter</h3></div>
         <div class="accordion" id="accordionExample">
-  <div class="accordion-item">
+  {/* <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
         Date
@@ -17,7 +17,7 @@ function SideMoviebar() {
       <input type="date" name="" id="" />
       </div>
     </div>
-  </div>
+  </div> */}
   <br />
   <div class="accordion-item">
     <h2 class="accordion-header">
@@ -27,12 +27,12 @@ function SideMoviebar() {
     </h2>
     <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body p-2 m-2">
-        <button class="btn btn-outline-primary">Telugu</button>
-        <button class="btn btn-outline-primary">English</button>
-        <button class="btn btn-outline-primary">Tamil</button>
-        <button class="btn btn-outline-primary">Hindi</button>
-        <button class="btn btn-outline-primary">Kannada</button>
-        <button class="btn btn-outline-primary">Malayalam</button>
+        <button class="btn btn-outline-primary p-1 m-1">Telugu</button>
+        <button class="btn btn-outline-primary p-1 m-1">English</button>
+        <button class="btn btn-outline-primary p-1 m-1">Tamil</button>
+        <button class="btn btn-outline-primary p-1 m-1">Hindi</button>
+        <button class="btn btn-outline-primary p-1 m-1">Kannada</button>
+        <button class="btn btn-outline-primary p-1 m-1">Malayalam</button>
       </div>
     </div>
   </div>
@@ -45,7 +45,14 @@ function SideMoviebar() {
     </h2>
     <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body p-2 m-2">
-        
+        <button class="btn btn-outline-primary p-1 m-1">Drama</button>
+        <button class="btn btn-outline-primary p-1 m-1">Action</button>
+        <button class="btn btn-outline-primary p-1 m-1">Thiller</button>
+        <button class="btn btn-outline-primary p-1 m-1">Romantic</button>
+        <button class="btn btn-outline-primary p-1 m-1">Comedy</button>
+        <button class="btn btn-outline-primary p-1 m-1">Crime</button>
+        <button class="btn btn-outline-primary p-1 m-1">Adventure</button>
+        <button class="btn btn-outline-primary p-1 m-1">Animation</button>
       </div>
     </div>
   </div>
@@ -58,12 +65,14 @@ function SideMoviebar() {
     </h2>
     <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
       <div class="accordion-body p-2 m-2">
-        
+        <button class="btn btn-outline-primary p-1 m-1">2D</button>
+        <button class="btn btn-outline-primary p-1 m-1">3D</button>
+        <button class="btn btn-outline-primary p-1 m-1">4DX</button>
       </div>
     </div>
   </div>
   <br />
-  <div class="accordion-item">
+  {/* <div class="accordion-item">
     <h2 class="accordion-header">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
         Price
@@ -74,7 +83,7 @@ function SideMoviebar() {
         
       </div>
     </div>
-  </div>
+  </div> */}
 </div>
 <br />
 <button type="button" class="btn btn-outline-danger">Browse by Cinemas</button>
